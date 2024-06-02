4a95839e041c29d009edfc1393dfda077c1dd6f3
Kashif <ahmed.kashif1001@gmail.com>
2024-06-02 23:46:38 +0500

    lesson2 basic student class

--- src/Lesson2/Basic.java
+++ src/Lesson2/Basic.java
@@ -1,8 +1,18 @@
 package Lesson2;
 
+class Student{
+    String name;
+    int age;
+    int roll_no;
+}
+
 
 public class Basic{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Student student1 = new Student();
+        student1.name = "Ahmed";
+        student1.age = 20;
+        student1.roll_no = 1;
+        System.out.println(student1.name);
     }
 }
