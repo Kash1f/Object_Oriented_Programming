fd70eccff35ddea70415dab1d6a85aeac56d97f8
Kashif <ahmed.kashif1001@gmail.com>
2024-06-06 23:46:19 +0500

    abstraction example2

---
+++ src/Abstraction/Example.java
@@ -0,0 +1,17 @@
+package Abstraction;
+
+abstract class Car{
+    abstract public void fueltype();
+    public void color(){
+        System.out.println("Black");
+    }
+}
+
+
+
+
+public class Example {
+    public static void main(String[] args) {
+        
+    }
+}
