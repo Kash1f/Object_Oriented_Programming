436f8f981358ecf88b5613a521e1df2077336b21
Kashif <ahmed.kashif1001@gmail.com>
2024-06-06 23:42:12 +0500

    polymorphism method overloading example2

--- src/Polymorphism/Polymorphism.java
+++ src/Polymorphism/Polymorphism.java
@@ -23,11 +23,36 @@ package Polymorphism;
 // }
 
 class Bank{
+    public void RateOfInterest(){
+        System.out.println("Generally rate of interest is 7%");
 
+    }
 }
 
-class Polymorphism{
+class MCB extends Bank{
+    //MCB has the RateOfInterest Method as it extends Bank Class
+
+    @Override //change the body i.e override it
+    public void RateOfInterest(){  
+        System.out.println("Rate of Interest of MCB is 6%");
+    }
+}
+
+class UBL extends Bank{
+    @Override
+    public void RateOfInterest(){
+        System.out.println("Generally rate of interest of UBL is 8%");
+}
+}
+
+public class Polymorphism {
     public static void main(String[] args) {
-        
+        Bank objBank = new Bank();
+        MCB obj = new MCB();
+        UBL obj1 = new UBL();
+
+        objBank.RateOfInterest();
+        obj.RateOfInterest();
+        obj1.RateOfInterest();
     }
 }
