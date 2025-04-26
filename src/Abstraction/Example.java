c592358fde007f747a865019c18b0d64cdf57ea6
Kashif <ahmed.kashif1001@gmail.com>
2025-04-26 23:11:10 +0500

    abstraction example

--- src/Abstraction/Example.java
+++ src/Abstraction/Example.java
@@ -13,11 +13,21 @@ class Toyota extends Car{
     }
 }
 
+class Honda extends Car{
+    public void fueltype(){
+        System.out.println("Gasoline");
+    }
+}
+
 
 public class Example {
     public static void main(String[] args) {
         Toyota corolla = new Toyota();
         corolla.fueltype();
         corolla.color();
+        Honda civic = new Honda();
+        civic.fueltype();
+        civic.color();
+
     }
 }
