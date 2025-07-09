7846a2d05bab7decfa5d2a714a0ac19c31764e35
Kashif <ahmed.kashif1001@gmail.com>
2025-07-10 00:56:58 +0500

    static variables example

---
+++ src/Static/StaticVariables/Example.java
@@ -0,0 +1,31 @@
+package Static.StaticVariables;
+
+// class Test {
+//     static int a = 10;
+
+//     void test() {
+//         int b = 20;
+//         System.out.println(b);
+//     }
+// }
+
+// public class Example {
+//     public static void main(String[] args) {
+//         System.out.println(Test.a);
+
+//         Test obj = new Test();
+//         obj.test();
+//     }
+// }
+
+class Test
+{
+    static int a = 10;
+}
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(Test.a);
+       
+    }
+}
--- src/Super/Example2.java
+++ src/Super/Example2.java
@@ -9,7 +9,7 @@ class B extends A{
 
     void show(int a){
     System.out.println(a); //30 since param a will receive arg 30 from main
-    System.out.println(this.a); //20 since this.a refers to the instance variable of class B
+    System.out.println(this.a); //20 since this.a refers to the instance variable of class B (current class)
     System.out.println(super.a); //10 since super.a refers to the instance variable of class A(parent class)
     }
 }
@@ -20,3 +20,14 @@ public class Example2 {
         obj1.show(30);
     }
 }
+
+
+/*
+
+Uses of Super Keyword:
+
+1. "super" keyword can be used to refer immediate parent class instance variable.
+2. "super" keyword can be used to invoke immediate parent class method.
+3. super() can be used to invoke immediate parent class constructor.
+
+*/
